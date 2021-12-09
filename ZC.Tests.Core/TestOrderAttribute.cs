bcc4038f38945d9e270b1bf9311c95593fa89b8f
Zdzislaw <zdzislaw.k@hotmail.com>
2021-12-08 22:05:52 -0300

    TestOrderer

---
+++ ZC.Tests.Core/TestOrderAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZC.Tests.Core
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+
+    public class TestOrderAttribute : Attribute
+    {
+        public int Order { get; private set; }
+
+        public TestOrderAttribute(int order) => Order = order;
+    }
+}
